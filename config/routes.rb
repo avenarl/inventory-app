98205acd42d7fb9298e14ac770ac73e7bcd5cea3
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2020-02-29 07:42:12 +0800

    Add new homepage for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+    devise_for :users
 
-  devise_for :users
+   root 'pages#homepage'
   resources :products, only: %i[index show edit destroy new create update destroy]
   resources :warehouses, only: %i[index show edit new create update destroy]
   resources :stocks, only: %i[index new show create]
