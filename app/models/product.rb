7c8b9a6b09031dec6a2d91cd1fdbd67698d1cddd
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2020-02-28 20:13:39 +0800

    Add order item with spec validations and associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   has_many :stocks
   has_many :warehouses, through: :stocks
+  has_many :order_items
   # Always have 1 line per attribute
   validates :name, presence: true
   validates :sku, presence: true, uniqueness: { case_sensitive: false}
