fc41df9279cfb8099516d6126d7c554ccd549825
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2020-02-28 20:22:29 +0800

    Update order to generate uuid

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ApplicationRecord
   has_many :stocks
-  has_many :warehouses, through: :stocks
   has_many :order_items
+  has_many :warehouses, through: :stocks
+  has_many :orders, through: :order_items
+
   # Always have 1 line per attribute
   validates :name, presence: true
   validates :sku, presence: true, uniqueness: { case_sensitive: false}
